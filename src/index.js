d4d8bbe40a3eaafd16c4818cca9323f8da1edfc8
Ayodeji olorunsogo <olorunso001@knights.gannon.edu>
2020-05-27 10:25:14 -0400

    confirm changes

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./components/App";
-
-
-
-ReactDOM.hydrate(<App initialData={window.initialData}/>, document.getElementById("root"));
